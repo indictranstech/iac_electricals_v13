63d63dd03d015426591cbd8afe1b3d7938ac3f77
AnilPise <anil.p@indictrans.in>
2021-09-02 10:58:17 +0530

    [Fix][Sales Order][Add freight Type with freight charge type and freight charge calculation based on percent or Amount on child table]

---
+++ iac_electricals/iac_electricals/custom_scripts/sales_order/sales_order.js
@@ -0,0 +1,39 @@
+frappe.ui.form.on("Sales Order", {
+	refresh: function(frm) {
+	}
+})
+
+frappe.ui.form.on('Sales Order Item',{
+	freight_charges_type :function(frm,cdt,cdn){
+		var d  = locals[cdt][cdn];
+		calculate_total(d);
+	},
+	freight_charges :function(frm,cdt,cdn){
+		var d  = locals[cdt][cdn];
+		calculate_total(d);
+	},
+	rate :function(frm,cdt,cdn){
+		var d  = locals[cdt][cdn];
+		calculate_total(d);
+	},
+	qty :function(frm,cdt,cdn){
+		var d  = locals[cdt][cdn];
+		calculate_total(d);
+	},
+});	
+
+var calculate_total = function(d) {
+	if(d.freight_charges_type == "Percent"){
+		var total_itm_charge  = d.rate*d.freight_charges/100 * d.qty
+		var _total_qty = d.qty*d.rate+ + total_itm_charge
+		frappe.model.set_value(d.doctype, d.name, "amount", _total_qty)
+	}else if(d.freight_charges_type == "Amount"){
+		var total_itm_charge  = d.freight_charges
+		var _total_qty = d.qty*d.rate+ total_itm_charge
+		frappe.model.set_value(d.doctype, d.name, "amount", _total_qty)
+	}else{
+		frappe.model.set_value(d.doctype, d.name, "freight_charges", 0)
+		frappe.model.set_value(d.doctype, d.name, "amount", d.qty*d.rate)
+	}
+
+}
---
+++ iac_electricals/iac_electricals/custom_scripts/sales_order/sales_order.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2021, IAC Electricals and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+def validate(self,method=None):
+	for i in self.items:
+		if i.freight_charges_type == "Percent" or i.freight_charges_type == "Amount":
+			if i.freight_charges == 0 or i.freight_charges == None:
+				frappe.throw("Please Enter Freight Charges for row "+ str(i.idx)+" in Item Table")
