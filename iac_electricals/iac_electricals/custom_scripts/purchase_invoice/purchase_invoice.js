23f612ffd372cfef5cfc0dc0ddabb181f0d3cd53
Dell <you@example.com>
2021-09-24 15:52:37 +0530

    [Feature][Purchase Invoice][Address Display On Purchase INvoice]

--- iac_electricals/iac_electricals/custom_scripts/purchase_invoice/purchase_invoice.js
+++ iac_electricals/iac_electricals/custom_scripts/purchase_invoice/purchase_invoice.js
@@ -3,5 +3,8 @@ frappe.ui.form.on("Purchase Invoice", {
 		if(!frm.doc.delivery_date) {
 			erpnext.utils.copy_value_in_all_rows(frm.doc, cdt, cdn, "items", "project");
 		}
+	},
+	delivery_address: function(frm) {
+		erpnext.utils.get_address_display(frm, "delivery_address", "delivery_address_display");
 	}
 })
