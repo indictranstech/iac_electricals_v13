199f744d6af76ad2e45254b1d5ff8fc5ba35d5ff
AnilPise <anil.p@indictrans.in>
2021-09-28 16:14:29 +0530

    [Fix][Sales Invoice][Fix taxes and charges functionality on Sales Invoice]

--- iac_electricals/iac_electricals/custom_scripts/sales_invoice/sales_invoice.js
+++ iac_electricals/iac_electricals/custom_scripts/sales_invoice/sales_invoice.js
@@ -1,5 +1,20 @@
 frappe.ui.form.on("Sales Invoice", {
-	refresh: function(frm) {
+	validate: function(frm) {
+		if(frm.doc.item_charges == "Item Level Freight Charge"){
+			var freight_total_on_itm = 0
+			frm.doc.items.forEach(d => {
+				freight_total_on_itm = freight_total_on_itm + d.freight_charges_on_all_quantity
+			})
+			frm.set_value("freight_total", freight_total_on_itm);
+		}else{
+			frm.set_value("freight_total", 0);
+		}
+	},
+	item_charges:function(frm){
+		frm.set_value("freight_total", 0);
+		frm.set_value("taxes_and_charges", "");
+		cur_frm.clear_table("taxes");
+		cur_frm.refresh_fields();
 	}
 })
 
@@ -24,16 +39,19 @@ frappe.ui.form.on('Sales Invoice Item',{
 
 var calculate_total = function(d) {
 	if(d.freight_charges_type == "Percent"){
-		var total_itm_charge  = d.rate*d.freight_charges/100 * d.qty
-		var _total_qty = d.qty*d.rate+ total_itm_charge
-		frappe.model.set_value(d.doctype, d.name, "amount", _total_qty)
+		var total_itm_charge  = d.rate*d.freight_charges/100
+		frappe.model.set_value(d.doctype, d.name, "freight_charge_per_quantity", total_itm_charge)
+		var _on_all_itm_charge = d.qty * d.freight_charge_per_quantity
+		frappe.model.set_value(d.doctype, d.name, "freight_charges_on_all_quantity", _on_all_itm_charge)
 	}else if(d.freight_charges_type == "Amount"){
 		var total_itm_charge  = d.freight_charges
-		var _total_qty = d.qty*d.rate+ total_itm_charge
-		frappe.model.set_value(d.doctype, d.name, "amount", _total_qty)
+		frappe.model.set_value(d.doctype, d.name, "freight_charge_per_quantity", total_itm_charge)
+		var _on_all_itm_charge = d.qty * d.freight_charge_per_quantity
+		frappe.model.set_value(d.doctype, d.name, "freight_charges_on_all_quantity", _on_all_itm_charge)
 	}else{
 		frappe.model.set_value(d.doctype, d.name, "freight_charges", 0)
-		frappe.model.set_value(d.doctype, d.name, "amount", d.qty*d.rate)
+		frappe.model.set_value(d.doctype, d.name, "freight_charge_per_quantity", 0)
+		frappe.model.set_value(d.doctype, d.name, "freight_charges_on_all_quantity", 0)
 	}
 
 }
