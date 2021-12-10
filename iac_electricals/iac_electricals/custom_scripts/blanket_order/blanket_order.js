4492c94dae757bfe91da402c271523a9bccc8975
hrishikesh20 <hrishikesh.k@indictrans.in>
2021-12-10 10:08:34 +0530

    lead opportunity price_schedule blanket_order

--- iac_electricals/iac_electricals/custom_scripts/blanket_order/blanket_order.js
+++ iac_electricals/iac_electricals/custom_scripts/blanket_order/blanket_order.js
@@ -17,3 +17,36 @@ frappe.ui.form.on('Blanket Order Item',{
 cur_frm.cscript.custom_refresh= function(frm){
 	$('.inner-group-button').find("[data-label='Quotation']").hide();
 }
+
+frappe.ui.form.on('Blanket Order', {
+	refresh(frm) {
+		refresh_field("items");
+	},
+	hiring_order:function(frm) {
+	   if (frm.doc.hiring_order) {
+		frappe.call({
+			method: "iac_electricals.iac_electricals.custom_scripts.blanket_order.blanket_order.get_item",
+			args: {"price_schedule_":frm.doc.price_schedule_},
+			callback: function(r) {console.log('r',r.message[0],'r');
+				if (r.message){ console.log('Add Row in Items');
+				var a = cur_frm.add_child("items");    
+				a.item_code = r.message[0];
+				a.item_name = r.message[1];
+				a.qty = r.message[2];
+				a.rate = r.message[3];
+
+				};
+			cur_frm.refresh_fields("items");	
+		},
+	});
+
+	   
+		   
+	   
+		
+	} 
+	
+	
+ }
+ 
+})
